6186fbb0065d4c8f71f9b3e1a5fa88dab66d6617
Simon <2053104031@qq.com>
2025-11-21 17:05:24 +1100

    Add files via upload

---
+++ Text_Generation/Text_Generation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Run and test DIP hard mode for text generation
+echo "Running Probabilistic Watermarking with Hard Mode (Text Generation)..."
+python3 Probabilistic_Watermarking.py \
+    --assumption hard \
+    --output_dir ./gpt2-ptb-backdoor-dip-hard
+
+echo "Running Watermark Verification with Hard Mode (Text Generation)..."
+python3 DIP_Verification.py \
+    --assumption hard \
+    --model_path ./gpt2-ptb-backdoor-dip-hard/checkpoint-10000
+
+# Run and test DIP soft mode for text generation
+echo "Running Probabilistic Watermarking with Soft Mode (Text Generation)..."
+python3 Probabilistic_Watermarking.py \
+    --assumption soft \
+    --output_dir ./gpt2-ptb-backdoor-dip-soft
+
+echo "Running Watermark Verification with Soft Mode (Text Generation)..."
+python3 DIP_Verification.py \
+    --assumption soft \
+    --model_path ./gpt2-ptb-backdoor-dip-soft/checkpoint-10000
